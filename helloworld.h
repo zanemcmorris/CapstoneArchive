d0315ee8db241fc10560f069d295b29caf6636cb
zanemcmorris <zane.mcmorris@colorado.edu>
2023-10-19 23:34:56 -0600

    Checkpoint on reorg

--- helloworld.h
+++ helloworld.h
@@ -1,24 +1,35 @@
+#ifndef HELLOWORLD_H
+#define HELLOWORLD_H
+
+
 #include "my_gpio.h"
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
+// File system / storage
 #include <Preferences.h>
+#include "spiffs_helper.h"
 
 
-typedef enum{
-  CO2,
-  PRESSURE,
-  PPM1,
-  PPM25,
-  PPM40,
-  PPM10,
-  HUMIDITY,
-  TEMP,
-  VOC,
-  CO, 
-  NG
-}sensor_map;
+  typedef enum {
+    CO2,
+    PRESSURE,
+    PPM1,
+    PPM25,
+    PPM40,
+    PPM10,
+    HUMIDITY,
+    TEMP,
+    VOC,
+    CO,
+    NG
+  } sensor_map;
 
 #define LED_PIN_0 A3
 #define LED_PIN_1 A1
+
+
+#endif
+
+
--- my_gpio.h
+++ my_gpio.h
@@ -1,12 +1,16 @@
+#ifndef MY_GPIO_H
+#define MY_GPIO_H
+
+#include <Arduino.h>
+// #include <Wire.h>
 #include "driver/i2c.h"
 #include "driver/gpio.h"
-#include "Wire.h"
 #include <pas-co2-ino.hpp>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
 #include <SensirionI2CSen5x.h>
 
-/***************************************************************************
+  /***************************************************************************
   This is a library for the BMP3XX temperature & pressure sensor
 
   Designed specifically to work with the Adafruit BMP388 Breakout
@@ -23,12 +27,12 @@
   BSD license, all text above must be included in any redistribution
  ***************************************************************************/
 
-/*
+  /*
  * I2C-Generator: 0.3.0
  * Yaml Version: 2.1.3
  * Template Version: 0.7.0-112-g190ecaa
  */
-/*
+  /*
  * Copyright (c) 2021, Sensirion AG
  * All rights reserved.
  *
@@ -60,6 +64,20 @@
  */
 
 
-#define I2C_FREQ_HZ  400000                     
-#define PERIODIC_MEAS_INTERVAL_IN_SECONDS  10 
-#define PRESSURE_REFERENCE  900
+#define I2C_FREQ_HZ 400000
+#define I2C_SDA_PIN 21
+#define I2C_SCL_PIN 22
+#define PERIODIC_MEAS_INTERVAL_IN_SECONDS 10
+#define PRESSURE_REFERENCE 900
+// Standard GPIO Pin definitions
+#define LED_OUT_3 A3
+#define LED_OUT_2 A2
+#define LED_OUT_1 A1
+#define NGPin A0
+#define COPin A1
+
+  void setup_GPIO(void);
+  void read_all_sensors(float *ret_array, uint16_t array_size);
+
+#endif
+
